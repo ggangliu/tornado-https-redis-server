0b239bbddb8eabd0485ac3bec769cd022b24b217
ggang.liu <304241378@qq.com>
2018-02-23 15:54:39 +0800

    Support get/set data by Redis

--- tornado-https.py
+++ tornado-https.py
@@ -1,10 +1,40 @@
 # -*- coding=utf-8 -*-
 
-import os, json
-from tornado.ioloop import IOLoop
+import os
+import json
+import redis
 from tornado import gen, web
 from tornado import httpserver
 from tornado import ioloop
+
+#user_infos = (["0135lQ2x0NJYgj18oF2x0BjN2x05lQ20", "你好"], ["0135lQ2x0NJYgj18oF2x0BjN2x05lQ22", "多久发货"], ["0135lQ2x0NJYgj18oF2x0BjN2x05lQ23", "在吗"])
+
+# user_infos = ({'user': "0135lQ2x0NJYgj18oF2x0BjN2x05lQ20", 'msg': "你好", 'reply_msg': ''},
+#               {'user': "0135lQ2x0NJYgj18oF2x0BjN2x05lQ21", 'msg': "什么时候发货", 'reply_msg': ''},
+#               {'user': "0135lQ2x0NJYgj18oF2x0BjN2x05lQ22", 'msg': "请问在吗", 'reply_msg': ''})
+
+
+def get_data_from_redis():
+    pool = redis.ConnectionPool(host='111.230.107.188', port=6379, decode_responses=True)
+    r = redis.Redis(connection_pool=pool)
+    user_data = []
+    for user in r.keys('*:user'):
+        user_dic = {}
+        user_name = r.get(user)
+        user_dic['user'] = user_name
+        user_dic['msg'] = r.get(user_name + ':msg')
+        user_dic['reply_msg'] = r.get(user_name + ':reply_msg')
+        user_data.append(user_dic)
+
+    user_data = tuple(user_data)
+    print user_data
+    return user_data
+
+
+def write_data_to_redis(user_name, reply_msg):
+    pool = redis.ConnectionPool(host='111.230.107.188', port=6379, decode_responses=True)
+    r = redis.Redis(connection_pool=pool)
+    r.set(user_name + ':reply_msg', reply_msg)
 
 
 class IndexHandler(web.RequestHandler):
@@ -17,8 +47,8 @@ class IndexHandler(web.RequestHandler):
         value = self.get_argument('echostr', 'ggangliu')
         print 'Request:'
         print self.request
-        user_infos = (["0135lQ2x0NJYgj18oF2x0BjN2x05lQ20", "你好"], ["0135lQ2x0NJYgj18oF2x0BjN2x05lQ22", "多久发货"], ["0135lQ2x0NJYgj18oF2x0BjN2x05lQ23", "在吗"])
-        self.render("index.html", users=user_infos)
+        user_info = get_data_from_redis()
+        self.render("index.html", users=user_info)
         #self.write(value)
         #self.on_finish()
 
@@ -35,10 +65,15 @@ class CustomerServiceHandler(web.RequestHandler):
     """Handler customer message"""
     @gen.coroutine
     def get(self):
-        value = self.get_query_argument('echostr', 'ggangliu')
+        user_name = self.get_query_argument('user', 'ggangliu')
+        reply_msg = self.get_query_argument('reply_msg', 'ggangliu')
+        write_data_to_redis(user_name, reply_msg)
         print 'Request:'
         print self.request
-        self.write(value)
+        # self.render("index.html", users=user_infos)
+        self.write("\nuser: " + user_name)
+        self.write("\nreply_msg: " + reply_msg)
+        self.write("\n回复完成！")
         self.on_finish()
 
     @gen.coroutine
@@ -63,7 +98,7 @@ def test_main():
     settings = {
         "static_path": os.path.join(os.path.dirname(__file__), "static"),
     }
-    application = web.Application([(r"/", IndexHandler), (r"/message", CustomerServiceHandler), ], autoreload=True,  **settings)
+    application = web.Application([(r"/", IndexHandler), (r"/reply_msg", CustomerServiceHandler), ], autoreload=True,  **settings)
     server = httpserver.HTTPServer(application)
     server.listen(80)
     ioloop.IOLoop.instance().start()
--- tornado-redis.py
+++ tornado-redis.py
@@ -2,22 +2,62 @@
 
 import redis
 
+userInfos = ({'user': "0135lQ2x0NJYgj18oF2x0BjN2x05lQ20", 'msg': "你好", 'reply_msg': ''},
+             {'user': "0135lQ2x0NJYgj18oF2x0BjN2x05lQ21", 'msg': "什么时候发货", 'reply_msg': ''},
+             {'user': "0135lQ2x0NJYgj18oF2x0BjN2x05lQ22", 'msg': "请问在吗", 'reply_msg': ''})
+
+userData = []
 # host是redis主机，需要redis服务端和客户端都启动 redis默认端口是6379
 pool = redis.ConnectionPool(host='111.230.107.188', port=6379, decode_responses=True)
 r = redis.Redis(connection_pool=pool)
 
-
-r.set('who', 'ggangliu')  # key是"foo" value是"bar" 将键值对存入redis缓存
-print(r.get('who'))  # 取出键name对应的值
-print(type(r.get('who')))
+r.flushall()
+# r.set('who', 'ggangliu')  # key是"foo" value是"bar" 将键值对存入redis缓存
+# print(r.get('who'))  # 取出键name对应的值
+# print(type(r.get('who')))
 
 pipe = r.pipeline() # 创建一个管道
-pipe.set('name', 'ggangliu')
-pipe.set('role', 'sb')
-pipe.sadd('faz', 'baz')
-pipe.incr('num')    # 如果num不存在则vaule为1，如果存在，则value自增1
-pipe.execute()
+for user in userInfos:
+    user_name = user['user'].__str__()
+    pipe.set(user_name + ':user', user['user'])
+    pipe.set(user_name + ':msg', user['msg'])
+    pipe.set(user_name + ':reply_msg', user['reply_msg'])
 
-print(r.get("name"))
-print(r.get("role"))
-print(r.get("num"))
+# pipe.incr('num')    # 如果num不存在则vaule为1，如果存在，则value自增1
+pipe.execute()
+r.save()
+
+
+for user in r.keys('*:user'):
+    user_dic = {}
+    user_name = r.get(user)
+    user_dic['user'] = user_name
+    user_dic['msg'] = r.get(user_name + ':msg')
+    user_dic['reply_msg'] = r.get(user_name + ':reply_msg')
+    userData.append(user_dic)
+
+userData = tuple(userData)
+print type(userData)
+print userData
+
+
+def get_data_from_redis():
+    pool = redis.ConnectionPool(host='111.230.107.188', port=6379, decode_responses=True)
+    r = redis.Redis(connection_pool=pool)
+    user_data = []
+    for user in r.keys('*:user'):
+        user_dic = {}
+        user_name = r.get(user)
+        user_dic['user'] = user_name
+        user_dic['msg'] = r.get(user_name + ':msg')
+        user_dic['reply_msg'] = r.get(user_name + ':reply_msg')
+        user_data.append(user_dic)
+
+    user_data = tuple(user_data)
+    return user_data
+
+
+def write_data_to_redis(user_name, reply_msg):
+    pool = redis.ConnectionPool(host='111.230.107.188', port=6379, decode_responses=True)
+    r = redis.Redis(connection_pool=pool)
+    r.set(user_name + ':reply_msg', reply_msg)
