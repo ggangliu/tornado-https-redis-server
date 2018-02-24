0c79b1b78e31eda5bcbad030114dc599e1108b1e
ggang.liu <304241378@qq.com>
2018-02-24 16:30:27 +0800

    Add weapp support

--- tornado-https.py
+++ tornado-https.py
@@ -6,6 +6,7 @@ import redis
 from tornado import gen, web
 from tornado import httpserver
 from tornado import ioloop
+from tornado import httpclient
 
 #user_infos = (["0135lQ2x0NJYgj18oF2x0BjN2x05lQ20", "你好"], ["0135lQ2x0NJYgj18oF2x0BjN2x05lQ22", "多久发货"], ["0135lQ2x0NJYgj18oF2x0BjN2x05lQ23", "在吗"])
 
@@ -61,6 +62,52 @@ class IndexHandler(web.RequestHandler):
         self.on_finish()
 
 
+class WeAppHandler(web.RequestHandler):
+    @gen.coroutine
+    def get(self):
+        value = self.get_argument('echostr', 'ggangliu')
+        print 'Request:'
+        print self.request
+        # user_info = get_data_from_redis()
+        # self.render("index.html", users=user_info)
+        yield self.write(value)
+        self.on_finish()
+
+    @gen.coroutine
+    def post(self):
+        print 'Post:'
+        recive_data = []
+        try:
+            js_code = self.get_query_argument('js_code')
+            print 'js_code:' + js_code
+            url = 'https://api.weixin.qq.com/sns/jscode2session?appid=wx87ccff16470f4817&secret=06d30910bbcb3442f616ceb689297eb0&js_code=' + js_code + '&grant_type=authorization_code'
+            http_clent = httpclient.AsyncHTTPClient()
+            response = http_clent.fetch(httpclient.HTTPRequest(url, method="GET"))
+            print 'respon: ' + response
+            self.data_received(recive_data)
+            print 'recive_data: ' + recive_data
+
+        except web.MissingArgumentError:
+            print "No js_code"
+            pass
+
+        param = json.loads(self.request.body.decode('utf-8'))
+        print param
+        print param['FromUserName']
+        # print param['Content']
+        reply_data = {
+            "touser": param['FromUserName'],
+            "msgtype": "text",
+            "text": {
+                "content": "We创者欢迎您....<a href=\"http://www.qq.com\" data-miniprogram-appid=\"wx87ccff16470f4817\" data-miniprogram-path=\"pages/index/index\">点击跳小程序</a>"
+            }
+        }
+        # self.write("success")
+        reply_json = json.dumps(reply_data)
+        self.write(reply_json)
+        self.on_finish()
+
+
 class CustomerServiceHandler(web.RequestHandler):
     """Handler customer message"""
     @gen.coroutine
@@ -85,7 +132,7 @@ def main():
     settings = {
         "static_path": os.path.join(os.path.dirname(__file__), "static"),
     }
-    application = web.Application([(r"/", IndexHandler), (r"/message", CustomerServiceHandler), ], autoreload=True,  **settings)
+    application = web.Application([(r"/", IndexHandler), (r"/we_app", WeAppHandler), (r"/reply_msg", CustomerServiceHandler), ], autoreload=True,  **settings)
     server = httpserver.HTTPServer(application, ssl_options={
            "certfile": os.path.join(os.path.abspath("."), "server.crt"),
            "keyfile": os.path.join(os.path.abspath("."), "server.key"),
@@ -105,4 +152,4 @@ def test_main():
 
 
 if __name__ == '__main__':
-    test_main()
+    main()
